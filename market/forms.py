57ab2c7b0de1ddf29bb2c5a85cc86f141e683a0c
= <vincent.tetteh@turntabl.io>
2022-04-10 22:41:09 +0000

    created marketing platform

---
+++ market/forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
+
+
+class RegisterForm(FlaskForm):
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError("username already taken")
+
+    def validate_email_address(self, email_to_check):
+        user = User.query.filter_by(email_address=email_to_check.data).first()
+        if user:
+            raise ValidationError("email already taken")
+
+    username = StringField(label="User Name", validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label="Email", validators=[Email(), DataRequired()])
+    password1 = PasswordField(label="Password", validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label="Confirm Password", validators=[EqualTo("password1"), DataRequired()])
+    submit = SubmitField(label="submit")
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label="User Name", validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    submit = SubmitField(label="submit")
+
+
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label="Purchase Item!")
+
+
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label="Sell Item!")
